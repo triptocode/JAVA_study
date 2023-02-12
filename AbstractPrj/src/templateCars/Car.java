225f48029428939bb14312f6f61baf66b6ceab5f
triptocode <3580rio@gmail.com>
2023-02-12 16:15:03 +0900

    abstract: extends, @OverRide, static final

--- AbstractPrj/src/templateCars/Car.java
+++ AbstractPrj/src/templateCars/Car.java
@@ -1,23 +1,22 @@
 package templateCars;
 
 public abstract class Car {
-	
 	public abstract void drive();
 	public abstract void stop();
-	
 	public void startCar() {
 		System.out.println("시동을 킴");
 	}
-	
 	public void turnOff() {
 		System.out.println("시동을 끊다");
 	}
-	
+	public void washCar() {} // abstract로하면 {}는 모든 자식클래스들이 구현해야함
+	// 하위클래스에서 순서를 재 정의 할수없게 final
 	final public void run() {
 		startCar();
 		drive();
 		stop();
 		turnOff();
+		washCar();
 	}
 	
 
--- AbstractPrj/src/templateCars/DriverlessCar.java
+++ AbstractPrj/src/templateCars/DriverlessCar.java
@@ -1,7 +1,8 @@
 package templateCars;
 
 public class DriverlessCar extends Car {
-	
+	// 자식클래스가 부모abstract클래스 추상메서드 {}를 구현하는 이유: 
+	// 자식클래스가 부모 추상클래스의 추상메서드를 구체화해야 new로 객체생성가능해서
 	@Override
 	public void drive() {
 		System.out.println("self-driving");	
@@ -10,4 +11,8 @@ public class DriverlessCar extends Car {
 	public void stop() {
 		System.out.println("self-stop");
 	}
+	@Override
+	public void washCar() {
+		System.out.println("self-washing");
+	};
 }
