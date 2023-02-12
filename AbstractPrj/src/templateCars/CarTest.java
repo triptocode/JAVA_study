f025ae9e4a6fdca86b2a1b6b4b0ca6d5843eebae
triptocode <3580rio@gmail.com>
2023-02-12 14:38:41 +0900

    abstract: extends 상속,  @Override , static final , template 메서드

--- AbstractPrj/src/templateCars/CarTest.java
+++ AbstractPrj/src/templateCars/CarTest.java
@@ -3,12 +3,12 @@ package templateCars;
 public class CarTest {
 	public static void main(String[] args) {
 		
-		System.out.println("==== DriverlessCar info ====");
+		System.out.println("==== template 메서드방식: DriverlessCar info ====");
 		
 		Car aiCar =new DriverlessCar();
 		aiCar.run();
 		
-		System.out.println("==== DriverCar info ====");
+		System.out.println("==== template 메서드방식: DriverCar info ====");
 		
 		Car manualCar = new DriverCar();
 		manualCar.run();
