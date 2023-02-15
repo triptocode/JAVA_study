b99d0b2109bc628b58905c7488d18eeb7a6cb41d
triptocode <3580rio@gmail.com>
2023-02-16 05:07:06 +0900

    upcasting 유무 차이

--- UpcastingPrj/src/B.java
+++ UpcastingPrj/src/B.java
@@ -1,8 +1,13 @@
 class B extends A {
-	@Override
+	
+	// A,B클래스 모두에 있는 m()메서드에 @override 안써도 덮어쓰기는 가능!
+    // 그렇다면, @Override 어노테이션을 안써도 되는데 표기하는 이유? 설명의 가독성?과 명확한 코드설계? 
+	
+	@Override 
 	public void m() {
 		System.out.println("method of B");
 	};
 	
+	// mBonly()메서드는 B클래스에서만 사용하여 @Override표기시 오류
 	public void mBonly() {System.out.println("not A method");}
 }
