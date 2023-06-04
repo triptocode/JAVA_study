8546c8ae810e4aabcf581587f3eb5c25febbeb37
triptocode <3580rio@gmail.com>
2023-06-05 00:34:20 +0900

    IF(모든메서드가 추상메서드) /익명함수,람다(js 화살표함수랑비슷,add()재정의)

--- LambdaPrj/src/addablePkg/LambdaExpressionTest.java
+++ LambdaPrj/src/addablePkg/LambdaExpressionTest.java
@@ -1,20 +1,22 @@
 package addablePkg;
 
 public class LambdaExpressionTest {
-
+    // semicolor 위치- 오류주의
     public static void main(String[] args) {
-        // semicolor 위치- 오류주의
+
+        //익명클래스 - lambda 사용x
         Addable add1 = new Addable() {
             @Override
             public int add(int a, int b) {return (a+b);}
         };
         System.out.println(add1.add(10,10));
 
+        // lambda 사용 - 첫번째 방식
         Addable add2 = (int a, int b)->{ return(a+b);};
         System.out.println(add2.add(20,20));
 
+        // lambda 사용 - 두번째 방식
         Addable add3 = (a,b)->(a+b);
         System.out.println(add3.add(30,30));
-
     }
 }
