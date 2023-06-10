5c322a14e1dfb0d95cba59a99c7a12950fbc8a4e
triptocode <3580rio@gmail.com>
2023-06-10 21:51:09 +0900

    collection

--- ArrayListPrj/src/namePkg/NameTest.java
+++ ArrayListPrj/src/namePkg/NameTest.java
@@ -5,21 +5,28 @@ public class NameTest {
     public static void main(String[] args) {
 
         ArrayList<String> nameList = new ArrayList<String>();
+        ArrayList<Integer> ageList = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < 4; i++) {
             System.out.print("이름을 입력하세요>>");
             String inputName = scanner.next(); // 키보드로부터 이름 입력
-
             nameList.add(inputName); // ArrayList 컬렉션에 삽입
+
+            System.out.print("나이를 입력하세요>>");
+            int inputAge = scanner.nextInt(); // 키보드로부터 나이 입력
+            ageList.add(inputAge); // ArrayList 컬렉션에 삽입
         }
 
         // ArrayList에 들어 있는 모든 이름 출력
         for (int i = 0; i < nameList.size(); i++) {
             // ArrayList의 i 번째 문자열 얻어오기
-            String result = nameList.get(i);
-            System.out.print(result + " ");
-//            System.out.println(result);A
+            String nameElement = nameList.get(i);
+            System.out.print(nameElement + " ");
+
+            int ageElement = ageList.get(i);
+            System.out.print(ageElement + " ");
+
         }
         scanner.close();
 
