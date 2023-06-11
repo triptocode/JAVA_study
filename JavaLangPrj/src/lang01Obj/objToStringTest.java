256e3566ec64d20af552b2a05f7df82da2242666
triptocode <3580rio@gmail.com>
2023-06-11 20:50:14 +0900

    java.lang - Object클래스-toString메소드

---
+++ JavaLangPrj/src/lang01Obj/objToStringTest.java
@@ -0,0 +1,19 @@
+package lang01Obj;
+
+
+class MyClass1{}
+class MyClass2 { public String toString(){return "마이클래스2";}}
+//class MyClass2 extends Object{ public String toString(){return "마이클래스2";}}
+// 위의 MyClass1, 과 MyClass2 모두 Object를 extends하고 있고, 해당부분 생략가능.
+
+// MyClass2에서는 Object클래스의 toString메소드를 상속받고, override 재정의 --> 문자그대로 나옴 "마이클래스2"
+// MyClass1은 참조주소값으로 나옴 --> lang01Obj.MyClass1@1b6d3586
+
+public class objToStringTest {
+    public static void main(String[] args) {
+        MyClass1 c1 = new MyClass1();
+        MyClass2 c2 = new MyClass2();
+        System.out.println(c1); // lang01Obj.MyClass1@1b6d3586
+        System.out.println(c2); // 마이클래스2
+    }
+}
